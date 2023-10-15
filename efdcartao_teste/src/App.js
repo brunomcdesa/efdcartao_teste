08675920c8f7474fd38be3dad60fca7b418e3d25
Bruno Cardoso <bruno.cardoso@xbrain.com.br>
2023-10-15 11:55:21 -0300

    Inicia tela de login

--- efdcartao_teste/src/App.js
+++ efdcartao_teste/src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "../src/css/styles.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div classname="page-container">
+        <main classname="main-container">
+          <div classname="bgImage">
+            <div>
+              <div>
+
+              </div>
+              <div>
+                <header classname="cabecalho">
+                  <p classname="imgLogo">
+                    <img src="https://s3.amazonaws.com/efdcartao.public/public/img/logo.png" alt="EFD Cartoes" height="70"/>
+                    <h2>Login</h2>
+                    <p>Efetue o login, ou   
+                      <a href="https://hmlg.efdcartao.com.br/createAccount"> cadastre-se</a>
+                      para uma nova conta.
+                    </p>
+                  </p>
+                </header>
+                <body>
+                  <form id="formLogin" classname="formLogin">
+                    <div classname="usuario">
+                      <input type="text" name="email" placeholder="Usuário" />
+                    </div>
+                    <div classname="senha">
+                      <input tupe="password" name="senha" placeholder="senha" />
+                    </div>
+                    <div>
+                      <div classname="esqueceuSenha">
+                        <a href="href=https://hmlg.efdcartao.com.br/forgot">Esqueceu a senha?</a>
+                      </div>
+                      <div classname="submit">
+                        <button id="btnLogin" classname="btnLogin">
+                          <i>icone</i>
+                          Acessar
+                        </button>
+                      </div>
+                    </div>
+                  </form>
+                </body>
+              </div>
+            </div>
+          </div>
+        </main>
+        {/* <footer>
+          <a classname="float">whatsapp</a>
+        </footer> */}
+      </div>
   );
 }
 
